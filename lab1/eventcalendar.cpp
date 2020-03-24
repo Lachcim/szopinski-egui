de95ff0d9ea142a3829be6bf5eb5e44cb3f61f9b
Michał Szopiński <jajnick@hotmail.com>
2020-03-24 19:08:29 +0100

    Implement event calendar

--- lab1/eventcalendar.cpp
+++ lab1/eventcalendar.cpp
@@ -1,5 +1,21 @@
 #include <eventcalendar.h>
+#include <QPainter>
 
 EventCalendarWidget::EventCalendarWidget(QWidget* parent) : QCalendarWidget(parent) {
 
+}
+
+void EventCalendarWidget::paintCell(QPainter* painter, const QRect& rect, const QDate& date) const {
+    QCalendarWidget::paintCell(painter, rect, date);
+
+    if (eventDates.contains(date))
+        painter->fillRect(rect, QColor::fromRgb(128, 128, 255, 64));
+}
+
+void EventCalendarWidget::addEvent(const QDate& date) {
+    eventDates += date;
+}
+
+void EventCalendarWidget::removeEvent(const QDate& date) {
+    eventDates -= date;
 }
--- lab1/eventcalendar.h
+++ lab1/eventcalendar.h
@@ -2,12 +2,23 @@
 #define EVENTCALENDAR_H
 
 #include <QCalendarWidget>
+#include <QSet>
 
 class EventCalendarWidget : public QCalendarWidget {
     Q_OBJECT
 
     public:
         EventCalendarWidget(QWidget* parent = nullptr);
+
+    protected:
+        void paintCell(QPainter* painter, const QRect& rect, const QDate& date) const;
+
+    private:
+        QSet<QDate> eventDates;
+
+    public slots:
+        void addEvent(const QDate&);
+        void removeEvent(const QDate&);
 };
 
 #endif
