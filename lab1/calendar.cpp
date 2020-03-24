22ed304709e56987222d414ac24acd0329bb471e
Michał Szopiński <jajnick@hotmail.com>
2020-03-24 18:05:00 +0100

    Design basic calendar window

--- lab1/calendar.cpp
+++ lab1/calendar.cpp
@@ -1,6 +1,6 @@
 #include <calendar.h>
 
-Calendar::Calendar(QWidget *parent) : QMainWindow(parent)
+Calendar::Calendar(QWidget* parent) : QMainWindow(parent)
 {
     ui.setupUi(this);
 }
--- lab1/calendar.h
+++ lab1/calendar.h
@@ -8,7 +8,7 @@ class Calendar : public QMainWindow {
     Q_OBJECT
 
     public:
-        explicit Calendar(QWidget *parent = nullptr);
+        explicit Calendar(QWidget* parent = nullptr);
 
     private:
         Ui::Calendar ui;
