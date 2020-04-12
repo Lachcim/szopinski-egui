9810d92523acda9230437936c869164420ebb185
Michał Szopiński <jajnick@hotmail.com>
2020-04-12 16:41:29 +0200

    Remove month change buttons

--- lab2/Models/CalendarViewModel.cs
+++ lab2/Models/CalendarViewModel.cs
@@ -9,5 +9,26 @@ namespace SzopinskiCalendar.Models {
         public int StartWeekday;
         public int DayCount;
         public Dictionary<int,List<EventViewModel>> Events;
+
+        public string GetPrevMonth() {
+            int month = Month - 1;
+            int year = Year;
+            if (month == 0) {
+                month = 12;
+                year--;
+            }
+
+            return year + "-" + month.ToString().PadLeft(2, '0');
+        }
+        public string GetNextMonth() {
+            int month = Month + 1;
+            int year = Year;
+            if (month == 13) {
+                month = 1;
+                year++;
+            }
+
+            return year + "-" + month.ToString().PadLeft(2, '0');
+        }
     }
 }
