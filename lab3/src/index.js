6c72f2d57348dc15e1eda8850ffd703cdfe402d5
Michał Szopiński <jajnick@hotmail.com>
2020-05-25 23:41:45 +0200

    Start lab3: prepare hello world app

---
+++ lab3/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SzopinskiCalendar from './SzopinskiCalendar';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<SzopinskiCalendar />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
